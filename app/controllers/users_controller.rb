437a98635c4505af95cfde2ca4c8d835098bd83d
Patrick <pa@arthurfamily.ws>
2015-08-10 23:49:32 -0400

    got registration working and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,34 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id]) || current_user
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.json { render action: 'index', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email, :current_password, :password,
+      :password_confirmation
+    )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
-  root to: "users#index"
+  root to: "users#show"
+
+  resources :users, only: [:index,:show]
 
 end
